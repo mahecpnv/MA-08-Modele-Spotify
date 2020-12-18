3843fb47a8a88fc0a152de802b9390c2d1b82a03
Mahé <61788230+mahecpnv@users.noreply.github.com>
2020-12-18 09:21:40 +0100

    DFDF
    WFDF

---
+++ Rendu/SQLQuery1.sql
@@ -0,0 +1,15 @@
+/*
+	Author : Mahé Lavaud & Jean Bösch
+	Description : Database de Spotify pour le projet à deux.
+*/
+  
+-- Créer la base de données
+-- create database Spotify;
+
+-- Create TABLE User (
+-- id int primary key not null identity (1,1),
+-- pseudo varchar(15) not null,
+-- email varchar(45) not null,
+-- password varchar() not null,
+-- )
+

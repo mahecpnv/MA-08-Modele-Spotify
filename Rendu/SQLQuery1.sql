4391497d2ab5ffe1fc0739a522f4755ea3ab401a
Mahé <61788230+mahecpnv@users.noreply.github.com>
2021-01-08 10:30:28 +0100

    Update SQLQuery1.sql

--- Rendu/SQLQuery1.sql
+++ Rendu/SQLQuery1.sql
@@ -10,6 +10,6 @@
 -- id int primary key not null identity (1,1),
 -- pseudo varchar(15) not null,
 -- email varchar(45) not null,
--- password varchar() not null,
+-- password varchar(20) not null,
 -- )
 

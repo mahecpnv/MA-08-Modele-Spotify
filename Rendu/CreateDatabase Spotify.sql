7890a7c9e4f3a0e15322c5a9b717dd4432754198
Mahé <61788230+mahecpnv@users.noreply.github.com>
2021-01-15 14:17:41 +0100

    fdg

--- Rendu/CreateDatabase Spotify.sql
+++ Rendu/CreateDatabase Spotify.sql
@@ -26,7 +26,7 @@ CREATE DATABASE Spotify
  Username varchar(45) not null,
  Email varchar(45) not null,
  [Password] varchar(45) not null,
- Phonenumber int not null,
+ PhoneNumber int not null,
  PlaylistNumber int not null,
  SubscritionNumber int not null
  )
@@ -46,20 +46,20 @@ CREATE DATABASE Spotify
  --------------------------------
  Create TABLE Artist (
  id int primary key not null identity (1,1),
- Pseudo varchar(15) not null,
+ Username varchar(15) not null,
  Email varchar(45) not null,
  [Password] varchar(45) not null,
- Phonenumber int not null,
- Playlistnumber int not null,
- Subscribernumber int not null
+ PhoneNumber int not null,
+ PlaylistNumber int not null,
+ SubscriberNumber int not null
  )
 
  --------------------------------
- -- Céer la 4 ème table "Format"
+ -- Céer la 4 ème table "Formats"
  --------------------------------
  Create TABLE Formats (
  id int primary key not null identity (1,1),
- Formatname varchar(45) not null,
+ FormatName varchar(45) not null,
  MaxMusic int not null
  )
 
@@ -68,9 +68,9 @@ CREATE DATABASE Spotify
  --------------------------------
  Create TABLE Prenium (
  id int primary key not null identity (1,1),
- Packagetype varchar(45) not null,
+ PackageType varchar(45) not null,
  Cost int not null,
- Renewaldate datetime  not null
+ RenewalDate datetime  not null
  )
 
  --------------------------------
@@ -90,7 +90,7 @@ CREATE DATABASE Spotify
  Create TABLE BankData (
  id int primary key not null identity (1,1),
  IBAN int not null,
- Cardnumber int not null,
+ CardNumber int not null,
  CCV int not null,
  ExpirationDate datetime not null,
  Firstname varchar(45) not null,
@@ -103,7 +103,7 @@ CREATE DATABASE Spotify
  Create TABLE Searchstyle (
  id int primary key not null identity (1,1),
  Names varchar(45) not null,
- Musicstyle varchar(45) not null
+ MusicStyle varchar(45) not null
  )
 
  --------------------------------
